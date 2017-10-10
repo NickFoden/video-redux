743c24a6aaac485a337f743ba0b9964296ee21f7
Nick Foden <nickfoden@gmail.com>
2017-10-10 15:51:21 -0400

    Added src directory

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Create a new component. this component should produce html
+
+const App = function() {
+    return<div> Hi ! </div>;
+}
+//Take thsi component's generated html and put it on the page
